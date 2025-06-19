cf01396345309e55248066ebaa62137ea17517d9
codezenashish <ixtashish@gmail.com>
2025-06-19 22:08:29 +0530

    arguments objects

--- 13_arrofunction/index.js
+++ 13_arrofunction/index.js
@@ -25,19 +25,37 @@ const f4 = (userName) => {
 };
 // f4("Ashish choudhary");
 
-
 const f5 = (userName) => {
   return "f5 called with userName: " + userName;
 };
 // console.log(f5());
 // console.log(f5('Ashish choudhary'));
 
-
-function sayHello(fun){
+function sayHello(fun) {
   fun("Ashish choudhary");
 }
 
-sayHello((userName) => {return "Hello " + userName;});
+sayHello((userName) => {
+  return "Hello " + userName;
+});
 sayHello((userName) => {
   console.log("Hello " + userName);
 });
+
+// arguments object
+
+function f6() {
+  console.log(arguments[0]);
+}
+
+f6(12, 14);
+
+
+// 
+function f7() {
+  for (let k in arguments) {
+    console.log(k, arguments[0]);
+  }
+}
+
+f7(12, 14, 50, 70, 60);
